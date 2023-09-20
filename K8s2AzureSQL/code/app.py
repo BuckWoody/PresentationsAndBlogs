445071d74ba30eb602132276eea1fac4edf3f41c
Davide Mauri <damauri@microsoft.com>
2023-09-20 10:44:29 -0700

    updated the example more modern and pythonic

--- K8s2AzureSQL/code/app.py
+++ K8s2AzureSQL/code/app.py
@@ -1,45 +1,50 @@
 # Set up the libraries for the configuration and base web interfaces
-import configparser
+import os
+import json
 from flask import Flask
+from flask_restful import Resource, Api
+from dotenv import load_dotenv
 import pyodbc
 
-# Open the configuration file
-config = configparser.ConfigParser()
-config.read('config.ini')
+# Load the variables from the .env file
+load_dotenv()
 
-# Create the Flask Application
+# Create the Flask-RESTful Application
 app = Flask(__name__)
+api = Api(app)
 
 # Create connection to Azure SQL DB using the config.ini file values
-ServerName = config.get('Connection', 'SQL_SERVER_ENDPOINT')
-DatabaseName = config.get('Connection', 'SQL_SERVER_DATABASE')
-UserName = config.get('Connection', 'SQL_SERVER_USERNAME')
-PasswordValue = config.get('Connection', 'SQL_SERVER_PASSWORD')
+server_name = os.getenv('SQL_SERVER_ENDPOINT')
+database_name = os.getenv('SQL_SERVER_DATABASE')
+user_name = os.getenv('SQL_SERVER_USERNAME')
+password = os.getenv('SQL_SERVER_PASSWORD')
 
 # Connect to Azure SQL DB using the pyodbc package
 # Note: You may need to install the ODBC driver if it is not already there. You can find that at:
 # https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17
-connection = pyodbc.connect(f'Driver=ODBC Driver 17 for SQL Server;Server={ServerName};Database={DatabaseName};uid={UserName};pwd={PasswordValue}')
+connection = pyodbc.connect(f'Driver=ODBC Driver 17 for SQL Server;Server={server_name};Database={database_name};uid={user_name};pwd={password}')
 
-# Create the query and set the cursor object to hold the results
-cursor = connection.cursor()
-cursor.execute("SELECT [ProductID], [Name], [Description] FROM [SalesLT].[vProductAndDescription] ORDER BY [Name];")
+# Create the SQL query to run against the database
+def query_db():
+    cursor = connection.cursor()
+    # Cast is needed to corretly inform pyodbc of output type is NVARCHAR(MAX)
+    # Needed if generated json is bigger then 4000 bytes and thus pyodbc trucates it
+    # https://stackoverflow.com/questions/49469301/pyodbc-truncates-the-response-of-a-sql-server-for-json-query
+    cursor.execute("SELECT CAST(( SELECT [ProductID], [Name], [Description] FROM [SalesLT].[vProductAndDescription] WHERE Culture = 'EN' FOR JSON AUTO ) AS NVARCHAR(MAX)) AS JsonResult;")
+    result = cursor.fetchone()
+    cursor.close()
+    return result
 
-# Display the results
-strContent= "<table style='border:1px solid red'>"
-for row in cursor:
-    strContent= strContent+ "<tr>"
-    for dbItem in row:
-        strContent= strContent+ "<td>" + str(dbItem) + "</td>"
-    strContent= strContent+ "</tr>"
-strContent= strContent+ "</table>"
-connection.close()
+# Create the class that will be used to return the data from the API
+class Products(Resource):
+    def get(self):
+        result = query_db()
+        json_result = {} if (result == None) else json.loads(result[0])     
+        return json_result, 200
 
-# Set the Flask application to run on the standard ports - typically port 5000
-@app.route('/')
-@app.route('/home')
-def home():
-    return "<html><body>" + strContent + "</body></html>"
+# Set the API endpoint to the Products class
+api.add_resource(Products, '/products')
 
+# Start App on default Flask port 5000
 if __name__ == "__main__":
     app.run(debug=True)
